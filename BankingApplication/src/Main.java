0a0c14fc58b55e09bfd265ea92e9e67bdeb3291c
Nathan Punter <127739950+PunterN@users.noreply.github.com>
2023-03-28 18:13:43 +0100

    Update Main.java

--- BankingApplication/src/Main.java
+++ BankingApplication/src/Main.java
@@ -1,90 +1,113 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Account account = new Account();
-
-        System.out.print("Enter initial account name: ");
-        String name = scanner.nextLine();
-        account.setName(name);
-
-        System.out.print("Please enter initial deposit amount: ");
-        double initialDeposit = scanner.nextDouble();
-        account.setBalance(initialDeposit);
-
-        System.out.println("Account created");
-        System.out.println("Account name: " + account.getName());
-        System.out.println("Initial balance: £" + account.getBalance());
+        ArrayList<Account> accounts = new ArrayList<>();
 
         while (true) {
-            System.out.println("\nPlease select an option:");
-            System.out.println("1. Deposit");
+            System.out.println("\nSelect option:");
+            System.out.println("1. Create account");
             System.out.println("2. Withdraw");
-            System.out.println("3. View balance");
+            System.out.println("3. Deposit");
+
             System.out.println("4. Exit");
-            int option = scanner.nextInt();
 
-            switch (option) {
+            int choice = scanner.nextInt();
+
+            switch (choice) {
                 case 1:
-                    System.out.print("Please enter deposit amount: ");
-                    double depositAmount = scanner.nextDouble();
-                    account.deposit(depositAmount);
-                    System.out.println("Deposit successful.");
-                    System.out.println("Balance after deposit: £" + account.getBalance());
+                    System.out.print("Enter account name: ");
+                    String name = scanner.next();
+                    System.out.print("Enter initial balance: ");
+                    int balance = scanner.nextInt();
+                    Account newAccount = new Account(name, balance);
+                    accounts.add(newAccount);
+                    System.out.println("Account created");
                     break;
-
                 case 2:
-                    System.out.println("Balance available for withdrawl: £" + account.getBalance());
-                    System.out.print("Please enter withdraw amount: ");
-                    double withdrawAmount = scanner.nextDouble();
-                    account.withdraw(withdrawAmount);
-                    System.out.println("Withdrawal successful.");
-                    System.out.println("Balance after withdrawal: £" + account.getBalance());
-                    break;
+                    System.out.print("Enter account name: ");
+                    name = scanner.next();
+                    Account withdrawAccount = selectAccount(accounts, name);
+                    if (withdrawAccount == null) {
+                        System.out.println("Account doesnt exist");
+                    } else {
+                        System.out.print("Enter withdrawal amount: ");
+                        double amount = scanner.nextDouble();
+                        withdrawAccount.withdraw(amount);
+                        System.out.println("Withdrawal successful");
+                        System.out.println("New balance: " + withdrawAccount.getBalance());
 
+                    }
+                    break;
                 case 3:
-                    System.out.println("Balance: £" + account.getBalance());
-                    break;
 
+                    System.out.print("Enter accounts name: ");
+                    name = scanner.next();
+                    Account depositAccount = selectAccount(accounts, name);
+                    if (depositAccount == null) {
+                        System.out.println("Account doesnt exist");
+                    } else {
+                        System.out.print("Deposit amount: ");
+                        int amount = scanner.nextInt();
+                        depositAccount.deposit(amount);
+                        System.out.println("Deposit successful");
+                        System.out.println("New balance: " + depositAccount.getBalance());
+
+                    }
+                    break;
                 case 4:
-                    System.out.println("Thank you for using the banking application!");
                     System.exit(0);
                     break;
-
                 default:
-                    System.out.println("Invalid option. Please try again.");
+                    System.out.println("Invalid choice.");
+                    break;
             }
         }
+    }
+
+    public static Account selectAccount(ArrayList<Account> accounts, String name) {
+        for (Account account : accounts) {
+            if (account.getName().equals(name)) {
+                return account;
+            }
+        }
+        return null;
     }
 }
 
 class Account {
     private String name;
-    private double balance;
+    private double funds;
 
-    public void setName(String name) {
+    public Account(String name, double funds) {
         this.name = name;
+        this.funds = funds;
     }
+
     public String getName() {
         return name;
     }
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+
     public double getBalance() {
-        return balance;
+        return funds;
     }
 
     public void deposit(double amount) {
-        balance += amount;
+        funds += amount;
     }
 
     public void withdraw(double amount) {
-        if (balance < amount) {
-            System.out.println("Insufficient funds.");
+        if (funds < amount) {
+            System.out.println("Insufficient funds");
         } else {
-            balance -= amount;
+            funds -= amount;
+            System.out.println("Withdrawal successful");
+            System.out.println("New balance is " + funds);
+
         }
     }
+
+
 }
